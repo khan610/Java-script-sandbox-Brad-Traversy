54ebde8a578ab3d4f8276bdc6c03a707b9c77c05
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-16 00:02:10 +0200

    Object Literals

--- app.js
+++ app.js
@@ -128,24 +128,24 @@
 
 //
 
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22, 45, 33, 76, 54);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
+// const numbers = [43, 56, 33, 23, 44, 36, 5];
+// const numbers2 = new Array(22, 45, 33, 76, 54);
+// const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+// const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
 
-let val;
+// let val;
 
-// Get array length
-val = numbers.length;
-// Check if is array
-val = Array.isArray(numbers);
-// Get single value
-val = numbers[3];
-val = numbers[0];
-// Insert into array
-numbers[2] = 100;
-// Find index of value
-val = numbers.indexOf(36);
+// // Get array length
+// val = numbers.length;
+// // Check if is array
+// val = Array.isArray(numbers);
+// // Get single value
+// val = numbers[3];
+// val = numbers[0];
+// // Insert into array
+// numbers[2] = 100;
+// // Find index of value
+// val = numbers.indexOf(36);
 
 // // MUTATING ARRAYS
 // // Add on to end
@@ -161,29 +161,67 @@ val = numbers.indexOf(36);
 // // Reverse
 // numbers.reverse();
 
-// Concatenate array (spoji sve vrednosti u jedan array)
-val = numbers.concat(numbers2);
+// // Concatenate array (spoji sve vrednosti u jedan array)
+// val = numbers.concat(numbers2);
 
-// Sorting arrays
-val = fruit.sort();
-val = numbers.sort();
+// // Sorting arrays
+// val = fruit.sort();
+// val = numbers.sort();
 
-// Use the "compare function"
-val = numbers.sort(function (x, y) {
-  return x - y;
-});
+// // Use the "compare function"
+// val = numbers.sort(function (x, y) {
+//   return x - y;
+// });
 
-// Reverse sort
-val = numbers.sort(function (x, y) {
-  return y - x;
-});
+// // Reverse sort
+// val = numbers.sort(function (x, y) {
+//   return y - x;
+// });
 
-// Find
-function over50(num) {
-  return num > 50;
-}
+// // Find
+// function over50(num) {
+//   return num > 50;
+// }
 
-val = numbers.find(over50);
+// val = numbers.find(over50);
 
-console.log(numbers);
+// console.log(numbers);
+// console.log(val);
+
+const person = {
+  firstName: 'Steve',
+  lastName: 'Smith',
+  age: 30,
+  email: 'steve@aol.com',
+  hobbies: ['music', 'sports'],
+  address: {
+    city: 'Miami',
+    state: 'FL',
+  },
+  getBirthYear: function () {
+    return 2017 - this.age;
+  },
+};
+
+let val;
+
+val = person;
+// Get specific value
+val = person.firstName;
+val = person['firstName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.getBirthYear();
+
 console.log(val);
+
+const people = [
+  { name: 'John', age: 30 },
+  { name: 'Mike', age: 23 },
+  { name: 'Nancy', age: 44 },
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
