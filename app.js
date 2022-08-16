470210f79e705cdea3d977fdd70fd5c6cf0bfeaf
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-16 22:56:58 +0200

    Update

--- app.js
+++ app.js
@@ -252,38 +252,73 @@
 
 //
 
-const id = 100;
+// const id = 100;
 
-// EQUAL TO
-if (id == 100) {
-  console.log('CORRECT');
+// // EQUAL TO
+// if (id == 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if (id != 101) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if (id === 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE & TYPE
+// if (id !== 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // GREATER OR LESS THAN
+// if (id > 200) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+const color = 'yellow';
+
+if (color === 'red') {
+  console.log('Color is red');
+} else if (color === 'blue') {
+  console.log('Color is blue');
 } else {
-  console.log('INCORRECT');
+  console.log('Color is not red or blue');
 }
 
-// NOT EQUAL TO
-if (id != 101) {
-  console.log('CORRECT');
+// LOGICAL OPERATORS
+
+const name = 'Steve';
+const age = 25;
+
+// And &&
+if (age > 0 && age < 12) {
+  console.log(`${name} is a child`);
+} else if (age >= 13 && age <= 19) {
+  console.log(`${name} is a teenager`);
 } else {
-  console.log('INCORRECT');
+  console.log(`${name} is an adult`);
 }
 
-// EQUAL TO VALUE & TYPE
-if (id === 100) {
-  console.log('CORRECT');
+// OR ||
+if (age < 16 || age > 65) {
+  console.log(`${name} can not run in race`);
 } else {
-  console.log('INCORRECT');
+  console.log(`${name} is registered for the race`);
 }
 
-// EQUAL TO VALUE & TYPE
-if (id !== 100) {
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-}
-
-if (id) {
-  console.log(`The ID is ${id}`);
-} else {
-  console.log('NO ID');
-}
+// TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
