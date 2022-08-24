9ec454a2c8d8737efff18624135d60dda44e7142
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-24 19:53:34 +0200

    Update

--- app.js
+++ app.js
@@ -465,7 +465,33 @@
 //   console.log(cars[i]);
 // }
 
-// // FOREACH
-// cars.forEach(function (car) {
-//   console.log(car);
+// // // FOREACH
+// cars.forEach(function (car, index, array) {
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
 // });
+
+// MAP
+const users = [
+  { id: 1, name: 'Jonh' },
+  { id: 2, name: 'Sara' },
+  { id: 3, name: 'Karen' },
+  { id: 4, name: 'Steve' },
+];
+
+const ids = users.map(function (user) {
+  return user.id;
+});
+
+console.log(ids);
+
+// FOR IN LOOP
+const user = {
+  firstName: 'Jonh',
+  lastName: 'Doe',
+  age: 40,
+};
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
