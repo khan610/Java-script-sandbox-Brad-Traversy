99af14cb267cf88e82de723246e2ff24fe1ae028
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-27 19:18:09 +0200

    Traversing the DOM

--- app.js
+++ app.js
@@ -41,87 +41,143 @@
 
 // document.getElementById()
 
-console.log(document.getElementById('task-title'));
+// console.log(document.getElementById('task-title'));
 
-// Get things from the element
-console.log(document.getElementById('task-title').id);
-console.log(document.getElementById('task-title').className);
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-const taskTitle = document.getElementById('task-title');
+// const taskTitle = document.getElementById('task-title');
 
-taskTitle.style.background = '#333';
-taskTitle.style.color = '#fff';
-taskTitle.style.padding = '5px';
-// document.getelementById('task-title).style.display = 'none';
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // document.getelementById('task-title).style.display = 'none';
 
-// Change content
-taskTitle.textContent = 'Task List';
-taskTitle.innerText = 'My Tasks';
-taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-// document.querySelector()
+// // document.querySelector()
 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:last-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelect('li:nth-child(even)').style.background = '#f4f4f4';
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:last-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelect('li:nth-child(even)').style.background = '#f4f4f4';
 
-// document.getElementsByClassName
+// // document.getElementsByClassName
 
-const items = document.getElementsByClassName('collection-item');
-console.log(items);
-console.log(items[0]);
-items[0].style.color = 'red';
-items[3].textContent = 'Hello';
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-let listItems = document
-  .querySelector('ul')
-  .getElementsByClassName('collection-item');
+// let listItems = document
+//   .querySelector('ul')
+//   .getElementsByClassName('collection-item');
 
-console.log(listItems);
+// console.log(listItems);
 
-// document.getElementsByTagName
-const lis = document.getElementsByTagName('li');
-console.log(lis);
-items[0].style.color = 'red';
-items[3].textContent = 'Hello';
+// // document.getElementsByTagName
+// const lis = document.getElementsByTagName('li');
+// console.log(lis);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-// Convert HTML Collection into array
-lis = Array.from(lis);
+// // Convert HTML Collection into array
+// lis = Array.from(lis);
 
-lis.reverse();
+// lis.reverse();
 
-lis.forEach(function (li, index) {
-  console.log(li.className);
-  li.textContent = `${index}: Hello`;
-});
+// lis.forEach(function (li, index) {
+//   console.log(li.className);
+//   li.textContent = `${index}: Hello`;
+// });
 
-console.log(lis);
+// console.log(lis);
 
-// document.querySelectorAll
-const items = document.querySelectorAll('ul.collection li.collection-item');
+// // document.querySelectorAll
+// const items = document.querySelectorAll('ul.collection li.collection-item');
 
-items.forEach(function (item, index) {
-  item.textContent = `${index}: Hello`;
-});
+// items.forEach(function (item, index) {
+//   item.textContent = `${index}: Hello`;
+// });
 
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
+// const liOdd = document.querySelectorAll('li:nth-child(odd)');
+// const liEven = document.querySelectorAll('li:nth-child(even)');
 
-liOdd.forEach(function (li, index) {
-  li.style.background = '#ccc';
-});
+// liOdd.forEach(function (li, index) {
+//   li.style.background = '#ccc';
+// });
 
-for (let i = 0; i < liEven.length; i++) {
-  liEven[i].style.background = '#f4f4f4';
-}
+// for (let i = 0; i < liEven.length; i++) {
+//   liEven[i].style.background = '#f4f4f4';
+// }
 
-console.log(items);
+// console.log(items);
+
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+val = listItem;
+val = list;
+
+// Get child nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[3].nodeType;
+
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+// Get children element nodes
+val = list.children;
+val = list.children[0];
+list.children[0].textContent = 'Hello';
+// Children of children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children;
+
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.lastChild;
+val = list.firstElementChild;
+
+// Count child elements
+val = list.childElementCount;
+
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val =
+  listItem.nextElementSibling.nextElementSibling.previousElementSiblingSibling;
+
+// Get prev sibling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
+
+console.log(val);
