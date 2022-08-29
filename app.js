230487b55ef47a2a2fba31fcfaa2c2b5ea541845
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-29 03:23:01 +0200

    Update

--- app.js
+++ app.js
@@ -413,16 +413,61 @@
 
 // deleteItem.addEventListener('click', deleteItem);
 
-document.body.addEventListener('click', deleteItem);
+// document.body.addEventListener('click', deleteItem);
 
-function deleteItem(e) {
-  console.log('delete item');
-  console.log(e.target);
-  // if (e.target.parentElement.className === 'delete-item secondary-content') {
-  //   console.log('delete item');
-  // }
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    console.log('delete item');
-    e.target.parentElement.parentElement.remove();
+// function deleteItem(e) {
+//   console.log('delete item');
+//   console.log(e.target);
+//   // if (e.target.parentElement.className === 'delete-item secondary-content') {
+//   //   console.log('delete item');
+//   // }
+//   if (e.target.parentElement.classList.contains('delete-item')) {
+//     console.log('delete item');
+//     e.target.parentElement.parentElement.remove();
+//   }
+// }
+
+// set local storage item
+localStorage.setItem('name', 'John');
+localStorage.setItem('age', '30');
+
+// // set session storage item
+// sessionStorage.setItem('name', 'Beth');
+
+// // remove from storage
+// localStorage.removeItem('name');
+
+// get from storage
+const name = localStorage.getItem('name');
+const age = localStorage.getItem('age');
+
+// clear local storage
+localStorage.clear();
+
+console.log(name, age);
+
+document.querySelector('form').addEventListener('submit', function (e) {
+  const task = document.getElementById('task').value;
+
+  let tasks;
+
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-}
+
+  tasks.push(task);
+
+  localStorage.setItem('task', JSON.stringify(tasks));
+
+  alert('Task saved');
+
+  e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem(tasks));
+
+tasks.forEach(function (task) {
+  console.log(task);
+});
