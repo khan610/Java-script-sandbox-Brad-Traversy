46ce048087c1f9e73d678adf2e89b17cf61dcca6
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-31 23:15:13 +0200

    Update

--- app.js
+++ app.js
@@ -492,54 +492,102 @@
 // const brad = new Person('Brad', '9-10-1981');
 // console.log(brad.calculateAge());
 
-// String
+// // String
 
-const name1 = 'Jeff';
-const name2 = new String('Jeff');
+// const name1 = 'Jeff';
+// const name2 = new String('Jeff');
 
-//name2.foo = 'bar';
-//console.log(name2);
+// //name2.foo = 'bar';
+// //console.log(name2);
 
-console.log(typeof name2);
+// console.log(typeof name2);
 
-if (name1 === 'Jeff') {
-  console.log('YES');
-} else {
-  console.log('NO');
+// if (name1 === 'Jeff') {
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+// // Number
+
+// const num1 = 5;
+// const num2 = new Number(5);
+
+// console.log(num2);
+
+// // Boolean
+
+// const bool1 = true;
+// const bool2 = new Boolean(true);
+
+// // Function
+// const getSum1 = function (x, y) {
+//   return x + y;
+// };
+
+// const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+// console.log(getSum1(1, 1));
+
+// // Object
+// const john = { name: 'John' };
+// const john2 = new Object({ name: 'John' });
+// console.log(john2);
+
+// // Arrays
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = new Array(1, 2, 3, 4);
+
+// // Regular Expressions
+// const re1 = /\w+/;
+// const re2 = new RegExp('\\w+');
+
+// console.log(re2);
+
+//Object.prototype
+//Person.prototype
+
+// Person constructor
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+  this.calculateAge = function () {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
 }
 
-// Number
-
-const num1 = 5;
-const num2 = new Number(5);
-
-console.log(num2);
-
-// Boolean
-
-const bool1 = true;
-const bool2 = new Boolean(true);
-
-// Function
-const getSum1 = function (x, y) {
-  return x + y;
+// Calculate age
+Person.prototype.calculateAge = function () {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
 };
 
-const getSum2 = new Function('x', 'y', 'return 1 + 1');
+// Get full name
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
 
-console.log(getSum1(1, 1));
+// Gets Married
+Person.prototype.getMaried = function (newLastName) {
+  this.lastName = newLastName;
+};
 
-// Object
-const john = { name: 'John' };
-const john2 = new Object({ name: 'John' });
-console.log(john2);
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
 
-// Arrays
-const arr1 = [1, 2, 3, 4];
-const arr2 = new Array(1, 2, 3, 4);
+console.log(mary);
 
-// Regular Expressions
-const re1 = /\w+/;
-const re2 = new RegExp('\\w+');
+console.log(john.calculateAge());
 
-console.log(re2);
+console.log(mary.getFullName());
+
+mary.getsMaried('Smith');
+
+console.log(mary.getFullName());
+
+console.log(mary.hasOwnProperty('firstName'));
+console.log(mary.hasOwnProperty('getFullName'));
