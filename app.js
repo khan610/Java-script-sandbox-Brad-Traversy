5220e63c4a8115acc8d597e391e360d1e92e4f66
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-31 23:27:14 +0200

    Update

--- app.js
+++ app.js
@@ -547,47 +547,87 @@
 //Object.prototype
 //Person.prototype
 
-// Person constructor
-function Person(firstName, lastName, dob) {
+// // Person constructor
+// function Person(firstName, lastName, dob) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.birthday = new Date(dob);
+//   this.calculateAge = function () {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   };
+// }
+
+// // Calculate age
+// Person.prototype.calculateAge = function () {
+//   const diff = Date.now() - this.birthday.getTime();
+//   const ageDate = new Date(diff);
+//   return Math.abs(ageDate.getUTCFullYear() - 1970);
+// };
+
+// // Get full name
+// Person.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+
+// // Gets Married
+// Person.prototype.getMaried = function (newLastName) {
+//   this.lastName = newLastName;
+// };
+
+// const john = new Person('John', 'Doe', '8-12-90');
+// const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+// console.log(mary);
+
+// console.log(john.calculateAge());
+
+// console.log(mary.getFullName());
+
+// mary.getsMaried('Smith');
+
+// console.log(mary.getFullName());
+
+// console.log(mary.hasOwnProperty('firstName'));
+// console.log(mary.hasOwnProperty('getFullName'));
+
+function Person(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.birthday = new Date(dob);
-  this.calculateAge = function () {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
 }
 
-// Calculate age
-Person.prototype.calculateAge = function () {
-  const diff = Date.now() - this.birthday.getTime();
-  const ageDate = new Date(diff);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
+// Greeting
+Person.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName}`;
 };
 
-// Get full name
-Person.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
+const person1 = new Person('John', 'Doe');
+
+// console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
 };
 
-// Gets Married
-Person.prototype.getMaried = function (newLastName) {
-  this.lastName = newLastName;
-};
-
-const john = new Person('John', 'Doe', '8-12-90');
-const mary = new Person('Mary', 'Johnson', 'March 20 1978');
-
-console.log(mary);
-
-console.log(john.calculateAge());
-
-console.log(mary.getFullName());
-
-mary.getsMaried('Smith');
-
-console.log(mary.getFullName());
-
-console.log(mary.hasOwnProperty('firstName'));
-console.log(mary.hasOwnProperty('getFullName'));
+console.log(customer1.greeting());
