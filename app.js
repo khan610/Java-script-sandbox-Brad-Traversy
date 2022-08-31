ae22a26d093636d4a01fe19cd9c3a662fbd75593
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-08-31 19:44:34 +0200

    Update

--- app.js
+++ app.js
@@ -474,20 +474,72 @@
 
 // Constructors and the 'this' Keyword
 
-// Person constructor
-function Person(name) {
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  this.calcAge = function () {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
+// // Person constructor
+// function Person(name) {
+//   this.name = name;
+//   // this.age = age;
+//   this.birthday = new Date(dob);
+//   this.calcAge = function () {
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   };
+// }
+
+// // const brad = new Person('Brad');
+// // const john = new Person('John');
+
+// const brad = new Person('Brad', '9-10-1981');
+// console.log(brad.calculateAge());
+
+// String
+
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+
+//name2.foo = 'bar';
+//console.log(name2);
+
+console.log(typeof name2);
+
+if (name1 === 'Jeff') {
+  console.log('YES');
+} else {
+  console.log('NO');
 }
 
-// const brad = new Person('Brad');
-// const john = new Person('John');
+// Number
 
-const brad = new Person('Brad', '9-10-1981');
-console.log(brad.calculateAge());
+const num1 = 5;
+const num2 = new Number(5);
+
+console.log(num2);
+
+// Boolean
+
+const bool1 = true;
+const bool2 = new Boolean(true);
+
+// Function
+const getSum1 = function (x, y) {
+  return x + y;
+};
+
+const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+console.log(getSum1(1, 1));
+
+// Object
+const john = { name: 'John' };
+const john2 = new Object({ name: 'John' });
+console.log(john2);
+
+// Arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+console.log(re2);
