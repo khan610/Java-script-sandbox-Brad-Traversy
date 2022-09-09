0b3a172650c08cf1b8e40ea5b0737eac4a0c60ec
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-09 17:51:44 +0200

    Update

--- app.js
+++ app.js
@@ -1103,8 +1103,27 @@ re = /h*llo/i; // Matches any character 0 or more times
 re = /gre?a?y/i; // Optional character
 re = /gre?a?y\?/i; // Escape character
 
+// Brackets [] - Character Sets
+re = /gr[ae]y/i; // Must be an a or e
+re = /[GF]ray/i; // Must be an G or F
+re = /[^GF]ray/i; // Match anything except a G or F
+re = /[A-Z]ray/; // Match any uppercase letter
+re = /[a-z]ray/; // Match any lowercase letter
+re = /[A-Za-z]ray/; // Match any letter
+re = /[0-9][0-9]ray/; // Match any digit
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i; // Must occur exactly {m} amount of times
+
+re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times
+
+re = /Hel{2,}o/i; // Must occur at least {m} amount of times
+
+// Paretheses () - Grouping
+re = /^([0-9]x){3}$/;
+
 // String to match
-const str = 'Brad Hello World';
+const str = 'Hello';
 
 // Log Results
 const result = re.exec(str);
