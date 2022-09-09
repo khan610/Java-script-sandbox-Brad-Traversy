d0db732a4791079b9e72ade1c9e3bfa5ce829e05
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-09 17:02:36 +0200

    Update

--- app.js
+++ app.js
@@ -1053,9 +1053,9 @@
 
 // console.log('Program continues...');
 
-let re;
-re = /hello/;
-re = /hello/i; // i = case insensitive
+// let re;
+// re = /hello/;
+// re = /hello/i; // i = case insensitive
 // re = /hello/g; // Global search
 
 // console.log(re);
@@ -1085,6 +1085,37 @@ re = /hello/i; // i = case insensitive
 
 // replace() - Return new string with some or all matches of a pattern
 
-const str = 'Hello There';
-const newStr = str.replace(re, 'Hi');
-console.log(newStr);
+// const str = 'Hello There';
+// const newStr = str.replace(re, 'Hi');
+// console.log(newStr);
+
+let re;
+// Literal Characters
+re = /hello/;
+re = /hello/i;
+
+// Metacharacter Symbols
+re = /^h/i; // Must start with
+re = / world$/i; // Must ends with
+re = /^hello$/i; // Must begin and end with
+re = /h.llo/i; // Matches any ONE character
+re = /h*llo/i; // Matches any character 0 or more times
+re = /gre?a?y/i; // Optional character
+re = /gre?a?y\?/i; // Escape character
+
+// String to match
+const str = 'Brad Hello World';
+
+// Log Results
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re, str) {
+  if (re.test(str)) {
+    console.log(`${str} matched ${re.source}`);
+  } else {
+    console.log(`${str} doest NOT match ${re.source}`);
+  }
+}
+
+reTest(re, str);
