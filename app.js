9138aaf796563a3d005c18aae1db98ef5da21bb1
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-13 14:39:36 +0200

    Iterators and Generators

--- app.js
+++ app.js
@@ -1089,67 +1089,122 @@
 // const newStr = str.replace(re, 'Hi');
 // console.log(newStr);
 
-let re;
-// Literal Characters
-re = /hello/;
-re = /hello/i;
+// let re;
+// // Literal Characters
+// re = /hello/;
+// re = /hello/i;
 
-// Metacharacter Symbols
-re = /^h/i; // Must start with
-re = / world$/i; // Must ends with
-re = /^hello$/i; // Must begin and end with
-re = /h.llo/i; // Matches any ONE character
-re = /h*llo/i; // Matches any character 0 or more times
-re = /gre?a?y/i; // Optional character
-re = /gre?a?y\?/i; // Escape character
+// // Metacharacter Symbols
+// re = /^h/i; // Must start with
+// re = / world$/i; // Must ends with
+// re = /^hello$/i; // Must begin and end with
+// re = /h.llo/i; // Matches any ONE character
+// re = /h*llo/i; // Matches any character 0 or more times
+// re = /gre?a?y/i; // Optional character
+// re = /gre?a?y\?/i; // Escape character
 
-// Brackets [] - Character Sets
-re = /gr[ae]y/i; // Must be an a or e
-re = /[GF]ray/i; // Must be an G or F
-re = /[^GF]ray/i; // Match anything except a G or F
-re = /[A-Z]ray/; // Match any uppercase letter
-re = /[a-z]ray/; // Match any lowercase letter
-re = /[A-Za-z]ray/; // Match any letter
-re = /[0-9][0-9]ray/; // Match any digit
+// // Brackets [] - Character Sets
+// re = /gr[ae]y/i; // Must be an a or e
+// re = /[GF]ray/i; // Must be an G or F
+// re = /[^GF]ray/i; // Match anything except a G or F
+// re = /[A-Z]ray/; // Match any uppercase letter
+// re = /[a-z]ray/; // Match any lowercase letter
+// re = /[A-Za-z]ray/; // Match any letter
+// re = /[0-9][0-9]ray/; // Match any digit
 
-// Braces {} - Quantifiers
-re = /Hel{2}o/i; // Must occur exactly {m} amount of times
+// // Braces {} - Quantifiers
+// re = /Hel{2}o/i; // Must occur exactly {m} amount of times
 
-re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times
+// re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times
 
-re = /Hel{2,}o/i; // Must occur at least {m} amount of times
+// re = /Hel{2,}o/i; // Must occur at least {m} amount of times
 
-// Paretheses () - Grouping
-re = /^([0-9]x){3}$/;
+// // Paretheses () - Grouping
+// re = /^([0-9]x){3}$/;
 
-// Shorthand Character Classes
-re = /\w/; // Word character - alphanumeric or _
-re = /\w+/; // + = one or more
-re = /\W/; // Non-Word Character
-re = /\d/; // Match any digit
-re = /\d+/; // Match any digit 0 or more times
-re = /\D/; // Match any Non-digit
-re = /\s/; // Match white char
-re = /\S/; // Match non-whitespace char
-re = /Hell\b/i; // Word boundary
+// // Shorthand Character Classes
+// re = /\w/; // Word character - alphanumeric or _
+// re = /\w+/; // + = one or more
+// re = /\W/; // Non-Word Character
+// re = /\d/; // Match any digit
+// re = /\d+/; // Match any digit 0 or more times
+// re = /\D/; // Match any Non-digit
+// re = /\s/; // Match white char
+// re = /\S/; // Match non-whitespace char
+// re = /Hell\b/i; // Word boundary
 
-// Assertions
-re = /x(?=y)/; // Match x only if followed by y
-re = /x(?!y)/; // Match x only if NOT Followed by y
+// // Assertions
+// re = /x(?=y)/; // Match x only if followed by y
+// re = /x(?!y)/; // Match x only if NOT Followed by y
 
-// String to match
-const str = 'djfdjkshfkjdsfjksdhcxy';
+// // String to match
+// const str = 'djfdjkshfkjdsfjksdhcxy';
 
-// Log Results
-const result = re.exec(str);
-console.log(result);
+// // Log Results
+// const result = re.exec(str);
+// console.log(result);
 
-function reTest(re, str) {
-  if (re.test(str)) {
-    console.log(`${str} matched ${re.source}`);
-  } else {
-    console.log(`${str} doest NOT match ${re.source}`);
+// function reTest(re, str) {
+//   if (re.test(str)) {
+//     console.log(`${str} matched ${re.source}`);
+//   } else {
+//     console.log(`${str} doest NOT match ${re.source}`);
+//   }
+// }
+
+// reTest(re, str);
+
+// Iterator Example
+
+// function nameIterator(names) {
+//   let nextIndex = 0;
+
+//   return {
+//     next: function () {
+//       return nextIndex < names.length
+//         ? { value: names[nextIndex++], done: false }
+//         : { done: true };
+//     },
+//   };
+// }
+
+// // Create an array of names
+// const namesArr = ['Jack', 'Jill', 'John'];
+// // Init iterator and pass in the names array
+// const names = nameIterator(namesArr);
+
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+
+// Generator Example
+// function* sayNames() {
+//   yield 'Jack';
+//   yield 'Jill';
+//   yield 'John';
+// }
+
+// const name = sayNames();
+
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+
+// ID Creator
+function* createIds() {
+  let index = 0;
+
+  while (true) {
+    yield index++;
   }
 }
 
-reTest(re, str);
+const gen = createIds();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
