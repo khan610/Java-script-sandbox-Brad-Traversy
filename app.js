cfeb97f61bda4c0144d926839a620cf46f76863c
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-20 19:51:53 +0200

    Symbols

--- app.js
+++ app.js
@@ -1192,20 +1192,51 @@
 // console.log(name.next().value);
 
 // ID Creator
-function* createIds() {
-  let index = 0;
+// function* createIds() {
+//   let index = 0;
 
-  while (true) {
-    yield index++;
-  }
-}
+//   while (true) {
+//     yield index++;
+//   }
+// }
 
-const gen = createIds();
+// const gen = createIds();
 
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
-console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+
+const sym1 = Symbol();
+const sym2 = Symbol('sym2');
+
+// console.log(typeof sym2);
+
+// console.log(Symbol('123') === Symbol('123'))
+// console.log(`Hello ${sym1.toString()}`);
+
+// Unique Object Keys
+const KEY1 = Symbol();
+const KEY2 = Symbol('sym2');
+
+const myObj = {};
+
+myObj[KEY1] = 'Prop1';
+myObj[KEY1] = 'Prop2';
+myObj.key3 = 'Prop3';
+myObj.key4 = 'Prop4';
+
+console.log(myObj[KEY1]);
+console.log(myObj[KEY2]);
+
+// Sumbols are not enumerable in for...in
+// for (let i in myObj) {
+//   console.log(`${i}: ${myObj[i]}`);
+// }
+
+// Symbols are ignored by JSON.stringify
+console.log(JSON.stringify({ key: 'prop' }));
+console.log(JSON.stringify({ [Symbol('sym1')]: 'prop' }));
