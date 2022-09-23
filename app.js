901512eaf851502f4391266c283a85141935bf90
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-23 17:22:59 +0200

    SETS

--- app.js
+++ app.js
@@ -1243,50 +1243,151 @@
 
 // Destructuring Assignment
 
-let a, b;
-[a, b] = [100, 200];
-// Rest pattern
-[a, b, c, ...rest] = [100, 200, 300, 400, 500];
-({ a, b, ...rest } = { a: 100, b: 200, c: 300, d: 400, e: 500 });
+// let a, b;
+// [a, b] = [100, 200];
+// // Rest pattern
+// [a, b, c, ...rest] = [100, 200, 300, 400, 500];
+// ({ a, b, ...rest } = { a: 100, b: 200, c: 300, d: 400, e: 500 });
 
-// Array Destructuring
+// // Array Destructuring
 
-// const people = ['John', 'Beth', 'Mike'];
+// // const people = ['John', 'Beth', 'Mike'];
 
-// const [person1, person2, person3] = people;
+// // const [person1, person2, person3] = people;
+
+// // console.log(person1, person2, person3);
+
+// // Parse array returned from function
+// function getPeople() {
+//   return ['John', 'Beth', 'Mike'];
+// }
+
+// let person1, person2, person3;
+// [person1, person2, person3] = getPeople();
 
 // console.log(person1, person2, person3);
 
-// Parse array returned from function
-function getPeople() {
-  return ['John', 'Beth', 'Mike'];
+// // Object Destructuring
+
+// const person = {
+//   name: 'John Doe',
+//   age: 32,
+//   city: 'Miami',
+//   gender: 'Male',
+//   sayHello: function () {
+//     console.log('Hello');
+//   },
+// };
+
+// // Old ES5
+// // const name = person.name,
+// //   age = person.age,
+// //   city = person.city;
+
+// // New ES6 Destructuring
+// const { name, age, city } = person;
+
+// console.log(name, age, city);
+
+// sayHello();
+
+// MAPS = key-value pairs - can use ANY type as a key or value
+
+const map1 = new Map();
+
+// Set Keys
+const key1 = 'some string',
+  key2 = {},
+  key3 = function () {};
+
+// Set map values by key
+map1.set(key1, 'Value of key1');
+map1.set(key2, 'Value of key2');
+map1.set(key3, 'Value of key3');
+
+// Get values by key
+// console.log(map1.get(key1), map1.get(key2), map1.get(key3));
+
+// // Count values
+// console.log(map1.size);
+
+// ITERATING MAPS
+
+// // Loop using for...of to get keys and values
+// for (let [key, value] of map1) {
+//   console.log(`${key} = ${value}`);
+// }
+
+// // Iterate keys only
+// for (let key of map1.keys()) {
+//   console.log(key);
+// }
+
+// // Iterate kays only
+// for (let value of map1.values()) {
+//   console.log(value);
+// }
+
+// // Loop with forEach
+// map1.forEach(function (value, key) {
+//   console.log(`${key} = ${value}`);
+// });
+
+// // Convert to ARRAYS
+
+// // Create an array of the key value pairs
+// const keyValArr = Array.from(map1);
+// console.log(keyValArr);
+
+// // Create an array of the values
+// const valArr = Array.from(map1.values());
+// console.log(valArr);
+
+// // Create an array of the keys
+// const keyArr = Array.from(map1.keys());
+// console.log(keyArr);
+
+// SETS - Store unique Values of any type
+
+const set1 = new Set();
+
+// Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({ name: 'John' });
+set1.add(true);
+set1.add(100);
+
+// const set2 = new Set([1, true, 'string']);
+// console.log(set2);
+
+console.log(set1);
+
+// Get count
+console.log(set1.size);
+
+// Check for values
+console.log(set1.has(100));
+console.log(set1.has(50 + 50));
+console.log(set1.has({ name: 'John' }));
+
+// Delete from set
+set1.delete(100);
+
+console.log(set1);
+
+// ITERATING THROUGH SET
+
+// For..of
+for (let item of set1.keys()) {
+  console.log(item);
 }
 
-let person1, person2, person3;
-[person1, person2, person3] = getPeople();
+// ForEach Loop
+set1.forEach((value) => {
+  console.log(item);
+});
 
-console.log(person1, person2, person3);
-
-// Object Destructuring
-
-const person = {
-  name: 'John Doe',
-  age: 32,
-  city: 'Miami',
-  gender: 'Male',
-  sayHello: function () {
-    console.log('Hello');
-  },
-};
-
-// Old ES5
-// const name = person.name,
-//   age = person.age,
-//   city = person.city;
-
-// New ES6 Destructuring
-const { name, age, city } = person;
-
-console.log(name, age, city);
-
-sayHello();
+// CONVERT SET TO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
