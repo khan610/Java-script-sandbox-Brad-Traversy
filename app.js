49b408985c598b21cd135fe823d6c6c916098e12
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-03-08 16:13:29 +0100

    String Methods & Concatenation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const firstName = 'William';
 const lastName = 'Johnson';
 const age = 34;
+const str = 'Hello there my name is Khan';
+const tags = 'web design,web development, programming';
 
 let val;
 
@@ -21,4 +23,39 @@ val = "That's awesome, I can't wait";
 // Length
 val = firstName.length;
 
-console.log(val);
+// concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2];
+
+// indexOf()
+val = firstName.indexOf('l');
+val = firstName.indexOf('l');
+
+// charAt()
+val = firstName.charAt('4');
+// Get last char
+val = firstName.charAt(firstName.length - 1);
+
+// substring()
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Khan', 'Andrea');
+
+// includes()
+val = str.includes('foo');
+
+val = console.log(val);
