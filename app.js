7fb6e76108c5e5c324b7cd0695b7efa35d5821a3
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-03-09 18:54:38 +0100

    Functions

--- app.js
+++ app.js
@@ -261,44 +261,87 @@ const id = 100;
 // // TERNARY OPERATOR
 // console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
 
-const color = 'yellow';
+// const color = 'yellow';
 
-switch (color) {
-  case 'red':
-    console.log('Color is red');
-    break;
+// switch (color) {
+//   case 'red':
+//     console.log('Color is red');
+//     break;
 
-  case 'blue':
-    console.log('Color is blue');
-    break;
+//   case 'blue':
+//     console.log('Color is blue');
+//     break;
 
-  default:
-    console.log('Color is not red or blue');
-    break;
-}
+//   default:
+//     console.log('Color is not red or blue');
+//     break;
+// }
 
-switch (new Date().getDay()) {
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 2:
-    day = 'Tuesday';
-    break;
-  case 3:
-    day = 'Wednesday';
-    break;
-  case 4:
-    day = 'Thursday';
-    break;
-  case 5:
-    day = 'Friday';
-    break;
-  case 6:
-    day = 'Saturday';
-    break;
-}
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = 'Sunday';
+//     break;
+//   case 1:
+//     day = 'Monday';
+//     break;
+//   case 2:
+//     day = 'Tuesday';
+//     break;
+//   case 3:
+//     day = 'Wednesday';
+//     break;
+//   case 4:
+//     day = 'Thursday';
+//     break;
+//   case 5:
+//     day = 'Friday';
+//     break;
+//   case 6:
+//     day = 'Saturday';
+//     break;
+// }
 
-console.log(`Today is ${day}`);
+// console.log(`Today is ${day}`);
+
+// function greet(firstName, lastName) {
+//   return 'Hello ' + firstName + ' ' + lastName;
+// }
+
+// console.log(greet());
+
+// FUNCTION EXPRESIONS
+
+// const square = function (x = 3) {
+//   return x * x;
+// };
+
+// console.log(square());
+
+// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs (IFIS se cita)
+
+// (function () {
+//   console.log('IIFE Ran...');
+// })();
+
+// (function (name) {
+//   console.log('Hello ' + name);
+// })('Brad');
+
+// PROPERTY METHODS
+
+const todo = {
+  add: function () {
+    console.log('Add todo...');
+  },
+  edit: function (id) {
+    console.log(`Edit todo ${id}`);
+  },
+};
+
+todo.delete = function () {
+  console.log('Delete todo...');
+};
+
+todo.add();
+todo.edit(22);
+todo.delete();
