96b5ae5afdbdbc0f7c11f8e350e4b4dbbc9dfce8
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-03-10 23:49:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -329,19 +329,19 @@ const id = 100;
 
 // PROPERTY METHODS
 
-const todo = {
-  add: function () {
-    console.log('Add todo...');
-  },
-  edit: function (id) {
-    console.log(`Edit todo ${id}`);
-  },
-};
+// const todo = {
+//   add: function () {
+//     console.log('Add todo...');
+//   },
+//   edit: function (id) {
+//     console.log(`Edit todo ${id}`);
+//   },
+// };
 
-todo.delete = function () {
-  console.log('Delete todo...');
-};
+// todo.delete = function () {
+//   console.log('Delete todo...');
+// };
 
-todo.add();
-todo.edit(22);
-todo.delete();
+// todo.add();
+// todo.edit(22);
+// todo.delete();
